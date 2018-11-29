f3a108150a30899d1ca51dd51b4f76802466b82b
Ragmatical <37642718+Ragmatical@users.noreply.github.com>
2018-11-28 19:19:03 -0800

    Add files via upload

---
+++ index.js
@@ -0,0 +1,56 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+var mongoose = require('mongoose')
+var labelModel = require('./schema.js').getModel()
+var http = require('http');
+var path = require('path');
+var fs = require('fs')
+
+var app = express()
+    , dbUri = process.env. MONGODB_RUI || 'mongodb://127.0.0.1/knowledge'
+    , server = http.createServer(app)
+    , port = process.env.PORT ? parseInt(proces.env.PORT) : 8080;
+;
+
+function startServer() {
+  app.use(bodyParser.json({
+    limit: '16mb'
+  }))
+  app.use(express.static(path.join(__dirname, 'public')));
+
+  app.get('/', (req, res, next) => {
+    console.log(req.query)
+    var label = new labelModel({
+          date: new Date()
+        , url: req.query.url
+        , label: req.query.label
+
+    });
+    label.save()
+
+    res.redirect(req.query.url)
+  });
+
+
+  server.on('listening', () => {
+
+    /* Determining what the server is listening for */
+    var addr = server.address(),
+      bind = typeof addr === 'string' ?
+      'pipe ' + addr :
+      'port ' + addr.port;
+
+    /* Outputs to the console that the webserver is ready to start listenting to requests */
+    console.log('Listening on ' + bind);
+  });
+
+  /* Tells the server to start listening to requests from defined port */
+  server.listen(port);
+}
+startServer()
+
+mongoose.connect(dbUri, function(err){
+    if (err){
+        return console.log(err)
+    }
+})
---
+++ scraper.js
@@ -0,0 +1,70 @@
+const puppeteer = require('puppeteer')
+var mongoose = require('mongoose')
+var labelModel = require("./schema.js").getModel();
+var express = require('express');
+var bodyParser = require('body-parser');
+var mongoose = require('mongoose')
+var labelModel = require('./schema.js').getModel()
+var http = require('http');
+var path = require('path');
+var fs = require('fs')
+var app = express()
+    , dbUri = process.env.MONGODB_URI || 'mongodb://127.0.0.1/knowledge'
+    , server = http.createServer(app)
+    , port = process.env.PORT ? parseInt(proces.env.PORT) : 27018;
+;
+
+
+
+mongoose.connect(dbUri, function(err){
+	if (err){
+		return console.log(err)
+	}
+	console.log("Starting Process")
+	labelModel.find({image:null}, async function(err, labels) {
+  if(err) {
+  		console.log("Found an error")
+  		console.log(err)
+  		return
+  	}
+  if(labels.length === 0) {
+    console.log("No new links found")
+    return
+  }
+	console.log("Found sites needing scraping, opening browser")
+	openBrowser(err, labels)
+	})
+})
+
+async function openBrowser(err, labels){
+	const browser = await puppeteer.launch();
+	const page = await browser.newPage();
+	console.log("Opened the browser")
+	console.log("Starting for loop")
+		for (var i = 0; i < labels.length;) {
+			var label = labels[i]
+			var url = label.url
+      console.log(url)
+			label.image = await scrapeImage(browser, page, url);
+      label.text = scrapeText(browser,page,url)
+			label.save();
+			console.log("Scraped "+ ++i +" site(s)")
+	}
+  await browser.close()
+}
+
+async function scrapeText(browser,page, url) {
+  await page.goto(url)
+  const result = await page.evaluate(() => {
+    return document.querySelector('html').innerHTML
+  })
+  return result;
+}
+
+async function scrapeImage(browser, page, url) {
+	await page.goto(url);
+	const buffer = await page.screenshot({
+	  fullPage: true
+	});
+	return buffer;
+  }
