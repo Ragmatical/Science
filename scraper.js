9449d2492a34bfcc4bf26c32b7f78e3fbf273089
Ragmatical <joostin@s50-62-22-21.secureserver.net>
2018-12-02 23:37:11 +0000

    saving to database testing

--- scraper.js
+++ scraper.js
@@ -13,43 +13,53 @@ var app = express()
     , server = http.createServer(app)
     , port = process.env.PORT ? parseInt(proces.env.PORT) : 27018;
 ;
+var browser;
+
+async function startBrowser() {
+    browser = await puppeteer.launch({args:['--no-sandbox']});
+    console.log(dbUri);
+    mongoose.connect(dbUri, function(err){
+        if (err){
+            return console.log(err)
+        } 
+        check()
+    })
+}
 
 
-console.log(dbUri);
-mongoose.connect(dbUri, function(err){
-	if (err){
-		return console.log(err)
-	}
-	console.log("Starting Process")
-	labelModel.find({image:null}, async function(err, labels) {
-  if(err) {
-  		console.log("Found an error")
-  		console.log(err)
-  		return
-  	}
-  if(labels.length === 0) {
-    console.log("No new links found")
-    return
-  }
-	console.log("Found sites needing scraping, opening browser")
-	openBrowser(err, labels)
-	})
-})
+
+
+function check() {
+    console.log("Starting Process")
+    labelModel.find({image:null}, async function(err, labels) {
+        if(err) {
+            console.log("Found an error")
+            console.log(err)
+            return
+        }
+        if(labels.length === 0) {
+            console.log("No new links found")
+            return
+        }
+        console.log("Found sites needing scraping, opening browser")
+        openBrowser(err, labels)
+        setTimeout(check, 1000*3600)
+    })
+}
 
 async function openBrowser(err, labels){
-	const browser = await puppeteer.launch();
-	const page = await browser.newPage();
-	console.log("Opened the browser")
-	console.log("Starting for loop")
-		for (var i = 0; i < labels.length;) {
-			var label = labels[i]
-			var url = label.url
-      console.log(url)
-			label.image = await scrapeImage(browser, page, url);
-      label.text = scrapeText(browser,page,url)
-			label.save();
-			console.log("Scraped "+ ++i +" site(s)")
-	}
+    const page = await browser.newPage();
+    console.log("Opened the browser")
+    console.log("Starting for loop")
+    for (var i = 0; i < labels.length;) {
+        var label = labels[i]
+        var url = label.url
+        console.log(url)
+        label.image = await scrapeImage(browser, page, url);
+        label.text = scrapeText(browser,page,url)
+        label.save();
+        console.log("Scraped "+ ++i +" site(s)")
+    }
   await browser.close()
 }
 
@@ -62,9 +72,11 @@ async function scrapeText(browser,page, url) {
 }
 
 async function scrapeImage(browser, page, url) {
-	await page.goto(url);
-	const buffer = await page.screenshot({
-	  fullPage: true
-	});
-	return buffer;
-  }
+    await page.goto(url);
+    const buffer = await page.screenshot({
+        fullPage: true
+    });
+    return buffer;
+}
+
+startBrowser()
